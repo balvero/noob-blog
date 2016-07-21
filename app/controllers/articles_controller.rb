124698110ec96086a8e9ee3f0294ba806bed65d1
Bernard <bernard.alvero@gmail.com>
2016-07-21 17:10:07 +0800

    Added edit action, created find_article private method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -20,13 +21,31 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated.'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
 
   private
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def find_article
+      @article = Article.find(params[:id])
     end
 
 end

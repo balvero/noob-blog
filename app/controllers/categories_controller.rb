4ea939ce47ac96c78f28fffa66aa5eb55a9716d9
Bernard Alvero <bernard.alvero@gmail.com>
2016-08-12 07:46:00 +0800

    Add Categories, Template Clean up, Bug fix

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,36 @@
+class CategoriesController < ApplicationController
+  before_action :find_category, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = 'Category created successfully'
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+
+end

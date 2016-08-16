df4ef4ab56875fb5ea488fd9a1c2c7e51fb3cf03
Bernard Alvero <bernard.alvero@gmail.com>
2016-08-16 17:39:59 +0800

    Edit Navigation and Add ArticleCategories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = User.create(username: 'noobita', email: 'noob@ita.com', password: '12345678', admin: true)
+  end
+
   test 'get new category form and create category' do
+    sign_in_as(@user, '12345678')
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -13,6 +18,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test 'invalid category submission results in failure' do
+    sign_in_as(@user, '12345678')
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count'  do
@@ -21,5 +27,6 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/new'
     assert_select 'div.alert'
   end
+
 
 end

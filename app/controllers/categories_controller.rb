da2ba59946c591edb6aed26b2a67a4917d29f765
Bernard Alvero <bernard.alvero@gmail.com>
2016-08-16 13:41:48 +0800

    Template Tweak | Navigation Edit | Categories Controller Test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,10 @@
 class CategoriesController < ApplicationController
   before_action :find_category, only: [:show, :edit, :update, :destroy]
+  before_action :require_admin, except: [:index, :show]
 
 
   def index
-    @categories = Category.order('created_at desc').paginate(page: params[:page], per_page: 10)
+    @categories = Category.order('name').paginate(page: params[:page], per_page: 10)
   end
 
   def new
@@ -31,6 +32,13 @@ class CategoriesController < ApplicationController
 
   def find_category
     @category = Category.find(params[:id])
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:warning] = 'Only admins can perform that action.'
+      redirect_to categories_path
+    end
   end
 
 end
